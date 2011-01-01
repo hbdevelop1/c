9fd006f0937e467a32d07641421f9d161697dd2d
hbdevelop1 <hbdevelop1@gmail.com>
2011-01-01 19:09:35 +1300

    multi-threading: writer and readers: version 1.1: writer opens a file and puts into a buffer

--- mt/mt_writerReaders.cpp
+++ mt/mt_writerReaders.cpp
@@ -19,11 +19,17 @@ readers lock the resource;
 #include<thread>
 #include<mutex>
 #include<iostream>
+#include<fstream>
+
 using namespace std;
 
-void writing();
-void reading();
-mutex mtx;
+
+void writing(mutex &);
+void reading(mutex &);
+
+const int g_chunck = 200;
+char shared_buffer[g_chunck];
+
 
 template <class type_of_arg1>
 void output(type_of_arg1 a1)
@@ -40,7 +46,7 @@ void output(type_of_arg1 a1, type_of_remaining_args... args)
 /*
 ostream & operator<<(ostream & o, )
 {
-    lock_guard(mtx);
+    lock_guard(mtx_output);
     o << arg;
     return o;
 }
@@ -49,21 +55,24 @@ class A:public ostream
 public:
     ostream & operator<<(ostream & o, )
     {
-        lock_guard(mtx);
+        lock_guard(mtx_output);
         o << arg;
         return o;
     }
 }
 */
+
 int main()
 {
+    mutex mtx_output;
+
     const int number_of_readers = 5;
     //create writer thread;
-    thread w(writing);
+    thread w(writing, ref(mtx_output));
     thread r[number_of_readers];
 
     for(int i=0; i<number_of_readers; ++i)
-        r[i]=thread(reading);
+        r[i]=thread(reading, ref(mtx_output));
 
 
 
@@ -75,20 +84,48 @@ int main()
     return 0;
 }
 
-void writing()
+void writing(mutex &mtx)
 {
-    int i=10;
-    while(--i)
+    ifstream f("mt/mt_writerReaders_data", ifstream::binary);
+
+    if(!f.good())
     {
+        unique_lock<mutex> u(mtx);
+        output("problem in file\n");
+        u.unlock();
+        return;
+    }
+    f.seekg(0, f.end);
+    uint sizeoffile=f.tellg();
+    f.seekg(0, f.beg);
+
+    signed int size_still_to_write=sizeoffile;
+
+    while(size_still_to_write>0)
+    {
+        int chunck = std::min(g_chunck, size_still_to_write);
+
+        f.read(shared_buffer, chunck);
+
+        if(size_still_to_write == chunck)
+        { //the last small chunck should be null terminated, otherwise we see the left over from the previous content
+            shared_buffer[size_still_to_write]=0;
+        }
+
+        size_still_to_write -= chunck;
+
+
         //cout<<"writer thread "<<std::this_thread::get_id()<<endl;
         unique_lock<mutex> u(mtx);
-        output("writer thread ",std::this_thread::get_id(),"\n");
+        //output("writer thread ",std::this_thread::get_id(),"\n");
+        //output(shared_buffer);
+        cout << shared_buffer;
         u.unlock();
         this_thread::sleep_for(std::chrono::milliseconds(5));
     }
 }
 
-void reading()
+void reading(mutex &mtx)
 {
     int i=10;
     while(--i)
