15f87473bac66a8af5e0b4b947983b836f1d9448
hbdevelop1 <https://hbdevelop1@github.com>
2016-03-14 12:54:07 +1300

    create a thread for each client.

--- tcpip/server.cpp
+++ tcpip/server.cpp
@@ -1,3 +1,10 @@
+/*
+a thread is created for each client.
+the new thread is push to the v_threads
+I need to reuse ended threads.
+*/
+
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -10,16 +17,16 @@
 #include <time.h> 
 
 #include <iostream> 
+#include <thread> 
+#include <vector> 
 using namespace std;
 
+void fn_sendtime(int connfd);
 
 int main(int argc, char *argv[])
 {
     int listenfd = 0, connfd = 0;
     struct sockaddr_in serv_addr; 
-
-    char sendBuff[1025];
-    time_t ticks; 
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0); //creates an UN-named socket inside the kernel
     //AF_INET For Internet family of IPv4 addresses we use AF_INET.
@@ -29,7 +36,6 @@ int main(int argc, char *argv[])
 
 
     memset(&serv_addr, '0', sizeof(serv_addr));
-    memset(sendBuff, '0', sizeof(sendBuff)); 
 
     serv_addr.sin_family = AF_INET; //the family/domain
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);//the interface to listen on (in case the system has multiple interfaces to network)
@@ -45,22 +51,40 @@ int main(int argc, char *argv[])
 
     cout << "Server: i am is up and running. Waiting for client ...\n";
     //
+
+    std::vector<std::thread> v_threads;
+
     while(1)
     {
     	cout << "Server: waiting for a client to connect ...\n";
+
         connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
         //the server is put to sleep
         //it is awaiken when the three way TCP handshake* is complete for an incoming client request
         // connfd is the socket descriptor representing the client socket.
 
-    	cout << "Server: client in connection ...\n";
+    	//create thread;
+    	v_threads.push_back(std::thread(fn_sendtime,connfd)); 
+
+    	cout << "Server: client " << v_threads.size() << " is in connection ...\n";
 
 
-        ticks = time(NULL);
-        snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
-        write(connfd, sendBuff, strlen(sendBuff)); 
+    	//loop back;
+    	//when ending, join all of them, release resources
 
-        close(connfd);
         sleep(1);
      }
+}
+
+
+void fn_sendtime(int connfd)
+{
+    char sendBuff[1025];
+    time_t ticks; 
+
+	ticks = time(NULL);
+	snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
+	write(connfd, sendBuff, strlen(sendBuff)); 
+
+	close(connfd);
 }
