a1804b84e73f6d1a124b122e6e56ea72ea3e0686
hbdevelop1 <https://hbdevelop1@github.com>
2016-03-20 18:19:44 +1300

    algo arithmetic progression finished, accepts decreasing progression

--- algo/arithmeticProgression.cpp
+++ algo/arithmeticProgression.cpp
@@ -1,16 +1,29 @@
+/*
+
+you were given a number N as Input 
+then N numbers followed (where 3<=N<=2500). 
+These N numbers were part of an Arithmetic Progression (of size N+1) from which one number was removed. 
+So the task was to find that Missing number. For instance
+
+5
+1 3 5 9 11  
+
+The output is 7
+
+*/
+
+
 #include <iostream>
 #include <assert.h>
 using namespace std;
 
 
-bool IsMissing(int array[], int d, int i, int j)
+bool IsMissing(int array[], int d, int lb, int ub)
 {
 //to do: negative elements, decreasing serie, 
  
-	assert(array[i]< array[j]);
-
-	int nbrofelements_inarray = j - i + 1;
-	int nbrofelements_intheory = (array[j]-array[i])/d + 1;
+	int nbrofelements_inarray = ub - lb + 1;
+	int nbrofelements_intheory = (array[ub]-array[lb])/d + 1;
 
 	cout << "array's size be " << nbrofelements_intheory << ", and it is " << nbrofelements_inarray << endl;
 
@@ -63,16 +76,32 @@ int GetMissing(array, 2, 0, 5)
 */
 int main()
 {
-	int array[]={1,3,5,7,9,11};
+//	int array[]={1,3,5,7,9,11};
+	int array[]={11,9,7,5,3,1
+						//,-1000000
+						};
+/*
+	char tt= 1000;
+	int rr = 0x7fffffff;
+	int rrr = -1.e6;
+	cout << tt << hex << rr <<", "<< dec << rr << ", 0x"<< hex << rrr <<", "<< dec << rrr << endl; 
+*/
+	int len = sizeof(array)/sizeof(int);
+	if(len < 3 || len >2500 )
+	{
+		return -1;
+	}
 
-//	if(GetMissing(array, 2, 0, 5))
-/*	if(IsMissing(array, 2, 0, sizeof(array)/sizeof(int) - 1))
-		cout << "missing." << endl;
-	else
-		cout << "complete." << endl;
-		*/
+	int ub = array[len-1];
+	int lb = array[0];
+	if(ub< -1.e6 || ub> 1.e6 || lb< -1.e6 || lb> 1.e6 )
+	{
+		return -1;
+	}
 
-	int missing = f(array, 2, 0, sizeof(array)/sizeof(int) - 1);
+	int step = (array[len-1] - array[0]) / (len-1);
+
+	int missing = f(array, step, 0, len-1);
 	if(missing>-1)
 		cout << "missing number is :" << missing << endl;
 
