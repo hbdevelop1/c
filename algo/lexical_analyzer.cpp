f737682d3876c13ca3a8ae388b6f13b858cd9b6e
hbdevelop1 <https://hbdevelop1@github.com>
2016-03-25 22:12:29 +1300

    algo/lexical analyzer in object oriented way 2

--- algo/lexical_analyzer.cpp
+++ algo/lexical_analyzer.cpp
@@ -6,21 +6,40 @@ using namespace std;
 
 std::list<std::function<bool(char)> > expected_functions;
 
+bool expect_digit = false;
+bool expect_dot = false;
+bool expect_sign = false;
+
 namespace hb
 {
 bool dot(char c)
 {
-	return (c=='.');
+	bool dot_found = (c=='.');
+
+	if(dot_found)
+	{
+		expect_dot = false;  //dot encountered. don't expect it any more.
+		expect_sign = false; //remove the possiblity to accept a sign, now that we 've encountered a dot 
+	}
+
+	return dot_found;
 }
 
 bool sign(char c)
 {
-	return (c=='+' || c=='-');
+	bool sign_found = (c=='+' || c=='-');
+	if(sign_found)
+		expect_sign=false;
+
+	return sign_found;
 }
 
 bool digit(char c)
 {
-	return isdigit(c)!=0;
+	bool digit_found = (isdigit(c)!=0);
+	if(expect_sign && digit_found) //remove the possiblity to accept a sign, now that we 've encountered a digit 
+		expect_sign = false;
+	return digit_found;
 }
 } //hb
 
@@ -35,27 +54,52 @@ bool doesCharacterMatchExpected(char *p)
 
 bool lexical_analysis_conforms(char *str)
 {
-	while(*str)
+	int i=0;
+
+	expect_dot = true;
+	expect_digit=true;
+	expect_sign =true;
+
+	while(str[i])
 	{
-		if(!doesCharacterMatchExpected(str))
+		if(i==0)
+		{
+			expected_functions.clear();
+
+			if(expect_sign) 	expected_functions.push_back(hb::sign);
+			if(expect_digit)	expected_functions.push_back(hb::digit);
+			if(expect_dot) 		expected_functions.push_back(hb::dot);
+		}
+		else
+		{
+			expected_functions.clear();
+
+			if(expect_digit)	expected_functions.push_back(hb::digit);
+			if(expect_dot) 		expected_functions.push_back(hb::dot);
+		}
+		if(!doesCharacterMatchExpected(&str[i]))
 			return false;
 
-		++str;
+		++i;
 	}
 	return true;
 }
 
 
-int main()
+int main(int arc, char** argv)
 {
-	char str[]="+1";
+	if(arc != 2)
+	{
+		cout <<"enter a string to analyze." << endl;
+		return -1;
+	}
 
-	expected_functions.clear();
-	expected_functions.push_back(hb::sign);
-	expected_functions.push_back(hb::digit);
-	expected_functions.push_back(hb::dot);
+	char *str=argv[1];
 
-	lexical_analysis_conforms(str);
+	bool f = lexical_analysis_conforms(str);
+
+	f? cout << str << " conforms to lexic" << endl
+	 : cout << str << " does't conform to lexic" << endl;
 
 	return 0;
 }
