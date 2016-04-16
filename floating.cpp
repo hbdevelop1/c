c27b1973886896274961910d014266b6e53fde6f
hbdevelop1 <https://hbdevelop1@github.com>
2016-04-16 22:02:07 +1200

    floating format

---
+++ floating.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main ()
+{
+    float a =0.5f; //=0x3F000000
+    float b =0.25f; //expected=0x3E000000 got=0x3E800000
+    cout << "a="<<a<< endl;
+    return 0;
+}
+
+int main26 ()
+{
+    float r =0.f;  //=0x00000000
+    float a =0.1f; //=0x3dcccccd
+    float b =1.0f; //expected=0x3F800000
+    float c =2.0f; //expected: 1.mantissa x 2^1=1.0 x 2^(128-127) = 0x40000000
+    int nan_rep = 0x7fc00000;
+    float nan=*reinterpret_cast<float*>(&nan_rep);
+    float nan1=sqrt(-1);
+    cout << "nan="<<nan << endl;
+    cout << "nan1="<<nan1 << endl;
+    return 0;
+}
