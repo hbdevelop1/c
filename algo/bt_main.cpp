61bcdada8329d2aff4ab94107393e90797695498
hbdevelop1 <https://hbdevelop1@github.com>
2016-04-21 10:08:48 +1200

    binary tree:Add <<

---
+++ algo/bt_main.cpp
@@ -0,0 +1,18 @@
+
+#include "bt.h"
+
+
+int main()
+{
+    BT b;
+
+    b.Add(4);
+    b.Add(2);
+    b.Add(19);
+    b.Add(1);
+    b.Add(15);
+    b.Add(100);
+    b.Add(1);
+
+    cout << b << endl;
+}
