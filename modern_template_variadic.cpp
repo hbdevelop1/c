6495cda846c9070b1da2e63bad2ef7b2b7e2d920
hbdevelop1 <https://hbdevelop1@github.com>
2016-05-23 14:25:52 +1200

    modern C++ template variadic

---
+++ modern_template_variadic.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <memory>
+#include <iterator>
+#include <vector>
+using namespace std;
+
+
+template<class T1>
+void fun(T1 t1)
+{
+    cout << t1 << endl;
+}
+
+template<class T1, typename... Ts>
+void fun(T1 t1, Ts... ts )
+{
+    cout << t1<<",";
+    fun(ts...);
+}
+
+
+int main()
+{
+    fun(4);
+    char t=66;
+    fun(t);
+    char & rt=t; fun(t,rt,66,2);
+}
+
