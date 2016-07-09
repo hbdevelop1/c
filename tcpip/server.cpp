8e66822e27b28dc55efaf87a63047c018393de0b
hbdevelop1 <hbdevelop1@gmail.com>
2016-07-09 15:33:07 +1200

    tcpip:server

--- tcpip/server.cpp
+++ tcpip/server.cpp
@@ -35,17 +35,20 @@ int main(int argc, char *argv[])
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0); //creates an UN-named socket inside the kernel
     //AF_INET For Internet family of IPv4 addresses we use AF_INET.
-    //‘SOCK_STREAM’ the transport layer protocol needed should be reliable ie it should have acknowledgement techniques. For example : TCP
-    //0 to let the kernel decide of the default protocol to use for this connection. For reliable connections, the default protocol used is TCP.
+    //‘SOCK_STREAM’ the transport layer protocol needed should be reliable ie it should have acknowledgement techniques.
+    //For example : TCP
+    //0 to let the kernel decide of the default protocol to use for this connection. For reliable connections, the default
+    //protocol used is TCP.
     // listenfd is integer known as socket descriptor.
 
 
     memset(&serv_addr, '0', sizeof(serv_addr));
 
     serv_addr.sin_family = AF_INET; //the family/domain
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);//listen to all ip addresses/interfaces 
-    //(in case the system has multiple interfaces on the network because wired interface has an IP different from the wireless interface)
     serv_addr.sin_port = htons(5000); //the port on which the server will wait for the client requests to come.
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);//listen to all ip addresses/interfaces
+    //(in case the system has multiple interfaces on the network because wired interface has an IP different from the wireless
+    //interface)
 
     bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
     //assigns the details specified in the structure ‘serv_addr’ to the socket listenfd.
@@ -71,32 +74,33 @@ int main(int argc, char *argv[])
 
         bool first_message_done = false;
         //find (get an iterator to) the first element in v_threads that is !joinable
-        auto iterator_th = find_if(begin(v_threads), end(v_threads),[&first_message_done](const auto & th)
-												        {
-												        	if(!first_message_done)
-												        	{
-												        		cout << "looking for a free thread \n";
-												        		first_message_done= true;
-												        	}
-												        	bool free = !th.joinable();
-												        	cout << "thread " << th.get_id() << " is";
-												        	free? cout << " free." << endl : cout << " not free." << endl;
-												        	return free;
-												        } );
-		if(iterator_th != v_threads.end())
-		{
-			*iterator_th = std::thread(fn_sendtime,connfd);
-			iterator_th->detach(); // <--------- important otherwise thread will not be reused next time even if it has ended
-    		cout << "Server: thread " << v_threads.size() << " is reused for the new client connection ...\n";
-		}
-		else
-		{
-	    	//create thread;
-	    	v_threads.push_back(std::thread(fn_sendtime,connfd));
-	    	auto iterator_th = v_threads.end()-1 ;
-	    	iterator_th->detach();  // <--------- important otherwise thread will not be reused next time even if it has ended
-	    	cout << "Server: new thread for the client in connection ...\n";
-	    }
+        auto iterator_th = find_if(begin(v_threads), end(v_threads),
+                                   [&first_message_done](const auto & th)
+                                    {
+                                            if(!first_message_done)
+                                            {
+                                                    cout << "looking for a free thread \n";
+                                                    first_message_done= true;
+                                            }
+                                            bool free = !th.joinable();
+                                            cout << "thread " << th.get_id() << " is";
+                                            free? cout << " free." << endl : cout << " not free." << endl;
+                                            return free;
+                                    } );
+        if(iterator_th != v_threads.end())
+        {
+            *iterator_th = std::thread(fn_sendtime,connfd);
+            iterator_th->detach(); // <--------- important otherwise thread will not be reused next time even if it has ended
+            cout << "Server: thread " << v_threads.size() << " is reused for the new client connection ...\n";
+        }
+        else
+        {
+            //create thread;
+            v_threads.push_back(std::thread(fn_sendtime,connfd));
+            auto iterator_th = v_threads.end()-1 ;
+            iterator_th->detach();  // <--------- important otherwise thread will not be reused next time even if it has ended
+            cout << "Server: new thread for the client in connection ...\n";
+        }
 
 
 
